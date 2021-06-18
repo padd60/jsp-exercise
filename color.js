d919459609c4127fa42d5114e77932f443eac75c
junghwankim <b6nqctxwfs@privaterelay.appleid.com>
2021-06-19 00:21:04 +0900

    jquery + css + onload

--- color.js
+++ color.js
@@ -1,19 +1,22 @@
 let Links = {
   setColor: function (color) {
-    const alist = document.querySelectorAll("a");
-    let i = 0;
-    while (i < alist.length) {
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    // const alist = document.querySelectorAll("a");
+    // let i = 0;
+    // while (i < alist.length) {
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $("a").css("color", color);
   },
 };
 let Body = {
   setColor: function (color) {
-    document.querySelector("body").style.color = color;
+    // document.querySelector("body").style.color = color;
+    $("body").css("color", color);
   },
   setBackGroundColor: function (color) {
-    document.querySelector("body").style.backgroundColor = color;
+    // document.querySelector("body").style.backgroundColor = color;
+    $("body").css("backgroundColor", color);
   },
 };
 function nightdayhandler(self) {
